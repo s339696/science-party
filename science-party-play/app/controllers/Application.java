b14b5fa797b46cc664d14af6c119d12b856569cc
Rosty <ch.weingaertner@gmail.com>
2016-02-19 08:38:49 +0100

    Bearbeitet: Main-Menüs prefinal             fehlerhafte Menü Einbindung in home.html

--- science-party-play/app/controllers/Application.java
+++ science-party-play/app/controllers/Application.java
@@ -3,6 +3,10 @@ package controllers;
 import models.ebean.User;
 import play.*;
 import play.mvc.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.libs.Json;
+import play.mvc.*;
 
 import views.html.*;
 
@@ -12,7 +16,31 @@ public class Application extends Controller {
         // Kurzer Datenbank Test
         System.out.println(User.find.byId(1L).username);
 
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Willkommen bei Science Party"));
+    }
+
+    public Result home() {
+        return ok(home.render("Willkommen bei Science Party"));
+    }
+
+    public Result login() {
+        return ok(login.render("Anmeldung"));
+    }
+
+    public Result register() {
+        return ok(register.render("Registrierung"));
+    }
+
+    public Result blog() {
+        return ok(blog.render("Blog"));
+    }
+
+    public Result events() {
+        return ok(events.render("Events"));
+    }
+
+    public Result kontakt() {
+        return ok(kontakt.render("Kontakt"));
     }
 
 }
