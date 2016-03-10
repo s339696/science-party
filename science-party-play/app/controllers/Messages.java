eaf9bd43dbc2aa72ad4fe20c4abf04c31c402aa8
Rosty <ch.weingaertner@gmail.com>
2016-03-10 12:55:22 +0100

    Neue Rout zu Messages

--- science-party-play/app/controllers/Messages.java
+++ science-party-play/app/controllers/Messages.java
@@ -1,10 +1,15 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.Result;
 
 /**
  * Handles all requests related to the messager.
  */
 public class Messages extends Controller {
+
+    public Result renderMessages() {
+        return ok(views.html.messages.messages.render("Nachrichten"));
+    }
 
 }
