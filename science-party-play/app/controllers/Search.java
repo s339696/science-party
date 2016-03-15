97e8aeb62f202296d730aad5e5f656e44a42016d
Erik Wolf <erik.wolf.29@gmail.com>
2016-03-15 16:45:16 +0100

    Es werden bei der Suche für neue Freunde jetzt Freunde, Anfragen und man selber rausgefiltert.

--- science-party-play/app/controllers/Search.java
+++ science-party-play/app/controllers/Search.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import manager.LoginManager;
 import models.ebean.User;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -11,10 +12,33 @@ import java.util.List;
  */
 public class Search extends Controller {
 
+    /**
+     * Returns the proper formatted search result of a user search for a friends request.
+     *
+     * @param sstring
+     * @return
+     */
     public Result searchUserForNewFriends(String sstring) {
-        return ok(views.html.search.userNewFriendSearch.render(searchUser(sstring)));
+        User user = LoginManager.getLoggedInUser();
+        if (user == null) {
+            return redirect(controllers.routes.Public.renderLoginPage());
+        }
+
+        List<User> resultUsers = searchUser(sstring);
+        resultUsers.remove(user);
+        resultUsers.removeAll(user.getFriends());
+        resultUsers.removeAll(user.getFriendRequests());
+
+        return ok(views.html.search.userNewFriendSearch.render(resultUsers));
     }
 
+    /**
+     * Returns a list of users which are fitting to the search string.
+     *
+     * FIXME: It's just working for firstname.
+     * @param sstring
+     * @return
+     */
     public List<User> searchUser(String sstring) {
         //String query = "WHERE MATCH (firstname,lastname,email) AGAINST ('" + sstring + "' IN BOOLEAN MODE)";
         List<User> resultUsers = User.find
